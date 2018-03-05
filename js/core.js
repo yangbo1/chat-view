2c65589e36c06324c27e88c6bacf867668b5c55a
杨波 <yang.bo@mobcb.com>
2018-03-05 10:52:29 +0800

    上线下线记录保存

--- js/core.js
+++ js/core.js
@@ -1,12 +1,12 @@
 //全局配置120.77.148.33
-var baseUrl = "http://120.77.148.33:9090";
-var wsServer = "ws://120.77.148.33:9090";
+var baseUrl = "http://localhost:9090";
+var wsServer = "ws://localhost:9090";
 var config = new function () {
     this.url = {
         register: baseUrl + "/service/v1/user/register",
         login: baseUrl + "/service/v1/user/login",
         server: wsServer + "/chatServer/",
-        frineds: baseUrl + "/service/v1/user/findFriendsById/",
+        // frineds: baseUrl + "/service/v1/user/findFriendsById/",
         frineds: baseUrl + "/service/v1/user/findFriendsByName",
     }
 }
